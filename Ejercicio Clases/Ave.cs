7d977db363407aa26e3b916fd928394e0ad39195
gurdian0614 <ivan.gurdian@outlook.com>
2024-01-20 08:56:32 -0600

    Agregando clase Ave

---
+++ Ejercicio Clases/Ave.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Clases
+{
+    internal class Ave
+    {
+        public string? Nombre;
+
+        public string? Color;
+
+        public string? Tamano;
+
+        public string? Familia;
+
+        public string? SexoBiologico;
+
+        public bool? EsVoladora;
+
+        //Constructor
+        public Ave()
+        {
+            EsVoladora = true;
+        }
+
+        public void imprimir()
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Tamaño: {Tamano}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Familia: {Familia}");
+            Console.WriteLine($"Sexo: {SexoBiologico}");
+            Console.WriteLine($"Es Voladora: {EsVoladora}");
+        }
+    }
+}
--- Ejercicio Clases/Program.cs
+++ Ejercicio Clases/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ejercicio_Clases;
+
+//Creamos un objeto
+Ave ave = new Ave();
+ave.Nombre = "Pajaro";
+ave.Tamano = "Pequeño";
+ave.Color = "Naranja";
+ave.Familia = "Ave";
+ave.SexoBiologico = "Macho";
+ave.imprimir();
+
+Ave ave2 = new Ave();
+ave2.Nombre = "Pinguino";
+ave2.Tamano = "Pequeño";
+ave2.Color = "Negro y Blanco";
+ave2.Familia = "Pinguinos";
+ave2.SexoBiologico = "Hembra";
+ave2.EsVoladora = false;
+ave2.imprimir();
+
+Ave ave3 = new Ave();
+ave3.Nombre = "Pinguino";
+ave3.Tamano = "Pequeño";
+ave3.Color = "Negro y Blanco";
+ave3.Familia = "Pinguinos";
+ave3.SexoBiologico = "Hembra";
+ave3.EsVoladora = false;
+Imprimir(ave3);
+
+void Imprimir(Ave ave)
+{
+    Console.WriteLine($"Nombre: {ave.Nombre}");
+    Console.WriteLine($"Tamaño: {ave.Tamano}");
+    Console.WriteLine($"Color: {ave.Color}");
+    Console.WriteLine($"Familia: {ave.Familia}");
+    Console.WriteLine($"Sexo: {ave.SexoBiologico}");
+    Console.WriteLine($"Es Voladora: {ave.EsVoladora}");
+}
